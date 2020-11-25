20b18d701b6f8e32e78704a46b7a9e846fdd4460
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-25 22:05:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+// Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+  let newArray = [];
+  newArray.push(arrayOfDrivers[0]);
+  newArray.push(arrayOfDrivers[1]);
+  return newArray;
+};
+
+const returnLastTwoDrivers = function(arrayOfDrivers) {
+  let newArray = [];
+    newArray.push(arrayOfDrivers[arrayOfDrivers.length - 2]);
+  newArray.push(arrayOfDrivers[arrayOfDrivers.length - 1]);
+  return newArray;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
